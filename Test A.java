c52a7d8a665045294f7ac7d8b40a472c65bfbb79
FRC Team 5510 <hamiteam5510@gmail.com>
2017-02-03 19:59:55 -0800

    Create Test A.java

---
+++ Test A.java
@@ -0,0 +1,17 @@
+private void highGoalSucks(){ 	// method for dumping balls
+	
+		Servo ballCollector= new Servo(1);
+	
+		private ServoBallCollector() {
+			if (xboxController.getRawButton(1)) {
+			ballCollector.setAngle(45);
+		}		
+	}
+	
+	private void iceClimbers(){
+		Servo climbingPick= new Servo(2);
+			if (xboxController.getRawAxis(3)>0.3) {
+			climbingPick.setAngle(75)
+	}
+
+}
